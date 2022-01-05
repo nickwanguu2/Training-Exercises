e586945fcaddcfdf9d962efbb3a2cb877706d0ae
Nick Wang <nick.wang.uu@gmail.com>
2022-01-05 13:20:27 -0700

    calc

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,17 +1,51 @@
 package com.company;
 
 public class Calculator {
+    private double num1;
+    private double num2;
+    public static String brand;
 
-    public double add(double num1, double num2){
+    public void setNum1(double num1){
+        if(num1 < 5){
+            this.num1 = 5;
+            return;
+        }
+        if(num1 > 20){
+            this.num1 = 20;
+            return;
+        }
+        this.num1 = num1;
+    }
+    public void setNum2(double num2){
+        if(num2 < 5){
+            this.num2 = 5;
+            return;
+        }
+        if(num2 > 20){
+            this.num2 = 20;
+            return;
+        }
+        this.num2 = num2;
+    }
+
+    public double getNum1(){
+        return num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public double add(){
         return num1 + num2;
     }
-    public double subtract(double num1, double num2){
+    public double subtract(){
         return num1 - num2;
     }
-    public double multiply(double num1, double num2){
+    public double multiply(){
         return num1 * num2;
     }
-    public double divide(double num1, double num2){
+    public double divide(){
         return num1 / num2;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,37 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        //exercise 1
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter 3 numbers.");
-        int int1 = scan.nextInt();
-        int int2 = scan.nextInt();
-        int int3 = scan.nextInt();
-        if(int1 > int2){
-            if(int1 > int3){
-                System.out.println(int1 + " is the greatest.");
-            } else {
-                System.out.println(int3 + " is the greatest.");
-            }
-        } else {
-            if(int2 > int3){
-                System.out.println(int2 + " is the greatest.");
-            } else {
-                System.out.println(int3 + " is the greatest.");
-            }
-        }
-        //exercise 3
-        System.out.println("Enter the max.");
-        int max = scan.nextInt();
-        int n = 1;
-        while (n <= max){
-            if (n % 2 == 0){
-                System.out.println(n + " is even.");
-            } else {
-                System.out.println(n + " is odd.");
-            }
-            n += 1;
-        }
+        Calculator calc =  new Calculator();
+        calc.setNum1(7);
+        calc.setNum2(11);
 
+        Calculator.brand = "Canon";
+
+        Calculator cal2 = new Calculator();
+        cal2.setNum1(6);
+        cal2.setNum2(30);
+
+        double s1 = calc.add();
+        double s2 = cal2.add();
+        double d1 = calc.subtract();
+        double p2 = cal2.multiply();
+        double q2 = cal2.divide();
+
+        System.out.println("Sum given by calculator1 with "+calc.getNum1()+" and "+ calc.getNum2()+": " +s1 + " and the brand is " + Calculator.brand);
+        System.out.println("Sum given by calculator2 with "+cal2.getNum1()+" and "+ cal2.getNum2()+": " +s2 + " and the brand is " + Calculator.brand);
+        System.out.println("Difference given by calculator1 with "+calc.getNum1()+" and "+ calc.getNum2()+": " +d1 + " and the brand is " + Calculator.brand);
+        System.out.println("Product given by calculator2 with "+cal2.getNum1()+" and "+ cal2.getNum2()+": " +p2 + " and the brand is " + Calculator.brand);
+        System.out.println("Quotient given by calculator2 with "+cal2.getNum1()+" and "+ cal2.getNum2()+": " +q2 + " and the brand is " + Calculator.brand);
     }
 }
