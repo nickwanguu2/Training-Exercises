ea2352c3ba33e8830174805dcdccae2cca284c47
Nick Wang <nick.wang.uu@gmail.com>
2022-01-11 12:37:35 -0700

    StudentStuff

---
+++ src/Jan11/Main.java
@@ -0,0 +1,12 @@
+package Jan11;
+
+
+import Jan11.service.ProcessStudent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProcessStudent processStudent = new ProcessStudent();
+        processStudent.process();
+    }
+}
